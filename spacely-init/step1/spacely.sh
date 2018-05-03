d082d1544719eca0a6c68a3750f4cb52e34e9468
brocksmedley <brocksmedley@gmail.com>
2018-05-02 23:17:41 -0700

    working spacely scripts

--- spacely-init/step1/spacely.sh
+++ spacely-init/step1/spacely.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # NOTE: a three-hash comment (###something), denotes a line that I still want to include but don't currently need (in cases of testing the script itself)
 
 
@@ -18,20 +20,24 @@ echo
 
 # copy tarball to L4T install dir (step1)
 echo "$st Copying BSP files to L4T install directory..."
-cp ./$img ./64_TX2/Linux_for_Tegra/
+mv $img ./64_TX2/Linux_for_Tegra_tx2/
+pwd
 echo "[DONE]"
 echo
 
 # extract tarball
 echo "$st Extracting firmware..."
-cd ./64_TX2/Linux_for_Tegra/
+cd ./64_TX2/Linux_for_Tegra_tx2/
+
+pwd
 tar -xzvf ./$img
 echo "[DONE]"
 echo
 
 # install Spacely BSP
 echo "$st Installing Spacely BSP..."
-cd ./CTI-L4T/
+cd CTI-L4T/
+pwd
 sudo ./install.sh
 echo "[DONE]"
 
@@ -42,11 +48,11 @@ echo "$st Flashing TX2 with BSP..."
 
 # wait for user to activate USB Recovery Mode on the TX2
 read -p "Please put the TX2 in Recovery mode and then press any key to continue..."
-cd ../
+cd ..
+pwd
 sudo ./flash.sh spacely-IMX274-3CAM mmcblk0p1
 
 ## END ###
 echo "Your TX2 should restart automatically when the installation finishes."
 echo "If not, please wait or reboot the TX2 manually."
-echo
-echo "When the TX2 is booted up, press ENTER to proceed to the next step..."
+echo "Next Step: Install software from Jetpack -- DO NOT FLASH OS."
--- spacely-init/step3/config_TX2.sh
+++ spacely-init/step3/config_TX2.sh
@@ -3,26 +3,39 @@
 # configures TX2 after being loaded with Jetpack & BSP
 
 # import cuda pgp key
-apt-key add /var/cuda-repo-9-0-local/7fa2af80.pub
+sudo apt-key add /var/cuda-repo-9-0-local/7fa2af80.pub
 
 # update system
-apt-get -y update
+sudo apt-get -y update
 #apt-get -y upgrade
 
 # conspicuous easter egg
 echo "We should use Salt(Stack) for this..."
 
-# install v4l-utils
-apt-get -y install v4l-utils
+# install pip
+sudo apt install -y python-pip
+# update pip
+sudo pip install --upgrade pip
 
-# build opencv4tegra
-  # get build_OpenCV.sh
-  # run it
-  # cd ~/opencv/build/
-  # make #(sanity check)
-  # sudo make install
-  # sudo apt-get install cmake-curses-gui
+# thanks https://gist.github.com/SSARCandy/fc960d8905330ac695e71e3f3807ce3d & Shu-Ping
+# install openCV dependencies
+sudo apt-get install -y build-essential
+sudo apt-get install -y cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev curl
+sudo apt-get install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt-get install -y qttools5-dev qttools5-dev-tools
 
+# run openCV build script
+sudo ./buildOpenCV.sh
+
+# install opencv to OS
+cd ~/opencv/build/
+sudo make install
+
+# config openCV to link library files
+sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+
+#sudo ldconfig
+echo "OpenCV installed."
 
 echo
-echo "All done. YAY!"
+echo "All done with this. Now install CUDA and TensorRT from Jetpack. YAY! (It might actually make more sense to do that stuff first."
